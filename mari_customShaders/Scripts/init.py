529528d7255bc37298e2c84564425d82223c76af
Diego Inácio <diegodci@gmail.com>
2014-11-03 14:54:35 -0200

    Update init.py

--- mari_customShaders/Scripts/init.py
+++ mari_customShaders/Scripts/init.py
@@ -7,7 +7,5 @@ PATH = os.path.abspath(os.curdir)
 mari.gl_render.registerCustomHeaderFile("DI_ADJUSTMENT_UTILS_GLSLH", "%s\\Shaders\\adjustment\\lib\\diMari_adjUtils.glslh" %(PATH))
 mari.gl_render.registerCustomCodeFile("DI_ADJUSTMENT_UTILS_GLSLC", "%s\\Shaders\\adjustment\\lib\\diMari_adjUtils.glslc" %(PATH))
 
-mari.gl_render.registerCustomAdjustmentLayerFromXMLFile("*di* custom/normalize", "%s\\Shaders\\adjustment\\normalize.xml" %(PATH))
-mari.gl_render.registerCustomAdjustmentLayerFromXMLFile("*di* custom/saturation", "%s\\Shaders\\adjustment\\saturation.xml" %(PATH))
-
-mari.gl_render.registerCustomAdjustmentLayerFromXMLFile("*di* custom/teste", "%s\\Shaders\\adjustment\\teste.xml" %(PATH))
+mari.gl_render.registerCustomAdjustmentLayerFromXMLFile("*di* custom/normalize", "%s\\Shaders\\adjustment\\diMariNormalize.xml" %(PATH))
+mari.gl_render.registerCustomAdjustmentLayerFromXMLFile("*di* custom/saturation", "%s\\Shaders\\adjustment\\diMariSaturation.xml" %(PATH))
