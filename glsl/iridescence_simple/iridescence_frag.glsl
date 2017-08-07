727b100bc92497fd6acdedb8e6571dd16ce4663d
Diego Inácio <diegodci@gmail.com>
2017-08-07 15:19:12 -0300

    update shader

--- glsl/iridescence_simple/iridescence_frag.glsl
+++ glsl/iridescence_simple/iridescence_frag.glsl
@@ -3,7 +3,7 @@ Author: Diego Inácio
 Shader: iridescence 01
 Version: 1.0.0
 Dev.: OpenGL Shading Language
-Date Upadated: JAN/09/2014
+Date Upadated: AUG/07/2017
 www.diegoinacio.com
 .....................................................................................................
 Fragment part of a simple iridescent shader using OpenGL Shading Language.
@@ -24,37 +24,45 @@ Resources:
 <OpenGL>
 http://www.opengl.org/
 ...................................................................................................*/
+#define PI		3.1415926535897931
 
-varying vec3 P;
-varying float fr;
+varying vec3	P;
+varying float	fr;
+
+const 	vec3	ofreq	=	{1.0, 1.0, 1.0},
+				nfreq	=	{1.0, 1.0, 1.0},
+				ooset	=	{0.0, 0.0, 0.0},
+				noset	=	{0.0, 0.0, 0.0};
+const 	float	nmult	=	1.0,
+				gamma	=	0.75,
+				minvl	=	0.0;
 
 float setRange(float value, float oMin, float oMax, float iMin, float iMax){
-	return iMin + ((value - oMin)/(oMax - oMin)) * (iMax - iMin);
+	return iMin + ((value - oMin)/(oMax - oMin))*(iMax - iMin);
 }
 
-float diNoise(vec3 pos){
-	//noise function to create irregularity
-	float mult = 1;
-	float oset = 45;		//offset
-	return	sin(pos.x*mult*2 + 12 + oset) + cos(pos.z*mult + 21 + oset) *
-			sin(pos.y*mult*2 + 23 + oset) + cos(pos.y*mult + 32 + oset) *
-			sin(pos.z*mult*2 + 34 + oset) + cos(pos.x*mult + 43 + oset);
+float diNoise(vec3 freq, vec3 offset){
+	// noise function to create irregularity
+	return	sin(2.0*PI*P.x*freq.x*2.0 + 12 + offset.x) + cos(2.0*PI*P.z*freq.x + 21 + offset.x)*
+			sin(2.0*PI*P.y*freq.y*2.0 + 23 + offset.y) + cos(2.0*PI*P.y*freq.y + 32 + offset.y)*
+			sin(2.0*PI*P.z*freq.z*2.0 + 34 + offset.z) + cos(2.0*PI*P.x*freq.z + 43 + offset.z);
 }
 
-vec3 iridescence(float orient){
-	//this function returns a iridescence value based on orientation
+vec3 iridescence(	float orient, float noiseMult,
+					vec3 freqA, vec3 offsetA, vec3 freqB, vec3 offsetB){
+	// this function returns a iridescence value based on orientation
 	vec3 irid;
-	float freq = 10;
-	float oset = 25;		//offset
-	float noiseMult = 1;
-	irid.x = abs(cos(orient*freq + diNoise(P)*noiseMult + 1 + oset));
-	irid.y = abs(cos(orient*freq + diNoise(P)*noiseMult + 2 + oset));
-	irid.z = abs(cos(orient*freq + diNoise(P)*noiseMult + 3 + oset));
+	irid.x = abs(cos(2*PI*orient*freqA.x + diNoise(freqB, offsetB)*noiseMult + 1 + offsetA.x));
+	irid.y = abs(cos(2*PI*orient*freqA.y + diNoise(freqB, offsetB)*noiseMult + 2 + offsetA.y));
+	irid.z = abs(cos(2*PI*orient*freqA.z + diNoise(freqB, offsetB)*noiseMult + 3 + offsetA.z));
 	return irid;
 }
 
 void main(){
-	float Ki = 1.0;			//iridescence multiplicator
-	vec4 iridColor = vec4(iridescence(fr), 1.0) * setRange(pow(1 - fr, 1/0.75), 0, 1, 0.1, 1);
-	gl_FragColor = iridColor*Ki;
+	vec3 _iridColor;
+	float _space, _incidence;
+	_space = pow(1.0 - fr, 1.0/gamma);
+	_incidence = setRange(_space, 0.0, 1.0, minvl, 1.0);
+	_iridColor = iridescence(fr, nmult, ofreq, ooset, nfreq, noset);
+	gl_FragColor = vec4(_iridColor, 1.0)*_incidence;
 }
